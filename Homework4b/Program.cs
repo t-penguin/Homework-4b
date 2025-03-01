4eb0e4cdefc1032275c8a696476c5aaf0d720f38
Joseph Collado <sourpina@gmail.com>
2025-03-01 18:10:09 -0500

    Added test for BankAccount creation

--- Homework4b/Program.cs
+++ Homework4b/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿// Test the creation of a BankAccount
+BankAccount account = new BankAccount("Joseph Collado", 100);
+Console.WriteLine("Created a new bank account");
+PrintAccountDetails(account);
+
+
+
+// Prints the account ID, balance, and transaction history
+void PrintAccountDetails(BankAccount account)
+{
+    Console.WriteLine($"Account ID: {account.AccountID}");
+    Console.WriteLine($"Account Balance: {account.Balance}");
+    Console.Write("Transaction History: ");
+    
+    List<decimal> transactionHistory = account.Transactions;
+    if (transactionHistory.Count == 0)
+    {
+        Console.WriteLine("N/A");
+        return;
+    }
+
+    for (int i = 0; i < transactionHistory.Count; i++)
+    {
+        decimal transaction = transactionHistory[i];
+        if (transaction > 0)
+            Console.Write($"+{transaction}");
+        else
+            Console.Write(transaction);
+
+        if (i != transactionHistory.Count - 1)
+            Console.Write(", ");
+        else
+            Console.WriteLine();
+    }
+}
