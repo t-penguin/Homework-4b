3faf11ea9ddd0c891b5b8727a32ffeef0218e525
Joseph Collado <sourpina@gmail.com>
2025-03-01 17:36:57 -0500

    Initial commit

---
+++ Homework4b/BankAccount.cs
@@ -0,0 +1,20 @@
+/* Joseph Collado
+
+UML
+Class: BankAccount
+
+Fields:
+- _accountID : string
+- _balance : decimal
+- _transactions : List<decimal>
+
+Properties:
++ AccountID : string
++ Balance : decimal
++ Transactions : List<decimal>
+
+Methods:
++ BankAccount(customerName : string, initBalance : decimal)
++ Deposit(decimal amount)
++ Withdraw(decimal amount) : bool
+*/
